39c29d0b78e11802435e23f36283d0fd8c782579
banghartOSU <banghart@oregonstate.edu>
2019-03-24 08:19:29 -0700

    changed one line

---
+++ Week_1_Assignments/animal.cpp
@@ -0,0 +1,16 @@
+/********************
+* Thomas Banghart
+* April , 2019
+*********************/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+	string faveAnimal;
+	cout << "Hello!"
+
+
+}
