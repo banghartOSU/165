3dd5a1326c4c6a48ba42c13fe1b7efb8c8d3951c
banghartOSU <banghart@oregonstate.edu>
2019-03-25 18:38:51 -0700

    Added change.cpp

---
+++ Week_1_Assignments/change.cpp
@@ -0,0 +1,19 @@
+/************************
+ * Thomas Banghart
+ * change.cpp
+ ***********************/
+
+#include <iostream>
+
+int main(){
+	int amount,
+		quarters, 
+		dimes,
+		nickels,
+		pennies;
+
+	std::cout << "Please enter an amount in cents less than a dollar.";
+
+	return 0;
+
+}
