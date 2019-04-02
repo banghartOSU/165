7cd7f8fd687dba0e3a9325805546a70092e0bfe6
banghartOSU <banghart@oregonstate.edu>
2019-04-02 08:29:28 -0700

    Completed average.cpp

--- week1/average.cpp
+++ week1/average.cpp
@@ -1,29 +1,38 @@
-/********************
-* Thomas Banghart
-* April , 2019
-*********************/
+/*****************************
+ * Author: Thomas Banghart
+ * Date: 04/02/2019
+ * Description: This program asks users to input six numbers,
+ * then returns the average of those numbers.
+ *****************************/
 
 #include <iostream>
 
-int main(){
-	double firstNum,
-			secondNum,
-			thirdNum,
-			fourthNum,
-			fifthNum,
-			result;
-	
-	std::cout << "Please enter five numbers." << std::endl;
-	std::cin >> firstNum;
-	std::cin >> secondNum;
-	std::cin >> thirdNum;
-	std::cin >> fourthNum;
-	std::cin >> fifthNum;
+int main () {
+	const int TOTAL_NUMBERS = 6;
+   double firstNumber,
+          secondNumber,
+          thirdNumber,
+          fourthNumber,
+          fifthNumber,
+          sixithNumber,
+	      result;
 
-	result = (firstNum + secondNum + thirdNum + fourthNum + fifthNum)/5;
+   //Ask users for six numbers that will be used
+   //to calculate the average
+   std::cout << "Please enter six numbers." << std::endl;
+   std::cin >> firstNumber;
+   std::cin >> secondNumber;
+   std::cin >> thirdNumber;
+   std::cin >> fourthNumber;
+   std::cin >> fifthNumber;
+   std::cin >> sixithNumber;
 
-	std::cout <<  "The average of those numbers is " << std::endl << std::fixed<< result <<std::endl;
+   //Computes the average and stores it in result
+   result = (firstNumber + secondNumber + thirdNumber
+	     + fourthNumber + fifthNumber + sixithNumber) / TOTAL_NUMBERS;
 
-	return 0;
+   std::cout << "The average of those numbers is:" << std::endl;
+   std::cout << result << std::endl;
 
+   return 0;
 }
