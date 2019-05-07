42c7a6a85329533ac7c8986e3d82439a691ad3c5
banghartOSU <banghart@oregonstate.edu>
2019-05-06 21:39:39 -0700

    Added week6

---
+++ week6/summer.cpp
@@ -0,0 +1,29 @@
+/*****************************
+ * Author: Thomas Banghart
+ * Date: 05/03/2019
+ * Description: summer recursively adds the values of a double array. 
+ *****************************/
+
+
+// #include<iostream>
+
+double summer(double arr[], int size){
+	double sum = 0.0;
+	
+	if(size > 0)
+		sum = arr[size-1] + summer(arr, size-1);
+
+	return sum;
+
+}
+
+
+
+// int main(){
+// 	const int SIZE = 3;
+// 	double arr[] = {1.0, 1.0, 1.0, 1.0, -1.0};
+
+// 	std::cout << summer(arr, SIZE) << std::endl;
+
+// 	return 0;
+// }
